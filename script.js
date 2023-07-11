aad87555f016ea58412f27f2371f9f93e5a284fa
Erjoh <erjik1902@gmail.com>
2023-07-11 15:33:22 +0600

    hw2

---
+++ script.js
@@ -0,0 +1,143 @@
+// let a = 'Erjan'
+// let b = 'Mederbekov'
+// let c = a.slice(1)
+// let d= b.slice(1)
+// let i= a.slice(-1)
+// let j= b.slice(-1)
+// let g= b.slice(1,7)
+//
+// console.log(j + c , i + d)
+
+// let a = 'Erjan'
+// let b = 'Mederbekov'
+// let c = a.slice(1)
+// let d= b.slice(7)
+// let i= a.slice(-1)
+// let j= b.slice(-1)
+// let g= b.slice(1,7)
+//
+// console.log(j + c , i + g + d.toUpperCase())
+
+// let a = 'Erjan'
+// let b = 'Mederbekov'
+// let c = a.slice(1)
+// let d= b.slice(1)
+//
+// console.log(b[b.length - 1] + c , a[a.length - 1] + d)
+
+// ===========1===========
+
+// let a= 'hello'
+// let b= 'Ivan'
+// let c = a.slice(1)
+// let d = b.slice(1)
+//
+// console.log(a[a.length -5].toUpperCase() + c , b[b.length -4].toLowerCase() + d)
+
+// ===========1===========
+
+// ===========2===========
+
+// let a = 'lorem ipsum dolor'
+// let b = a.length
+//
+// console.log(b)
+
+// ===========2===========
+
+// ===========3===========
+
+// let a = 'lorem ipsum dolor sit amet'
+// let b = a.slice(18,22)
+//
+// console.log(b + a.substr(0, 17) + ' ' + a.substr( 22))
+
+// ===========3===========
+
+// ===========4===========
+
+// let a = 'children'
+// let b = a[a.length - 1]
+// let c = a.slice(0,7)
+//
+// console.log(c.toUpperCase() + b)
+
+// ===========4===========
+
+// ===========5===========
+
+// let a = 'boss'
+// let b = a.slice(a.indexOf('s'))
+// let c = a.slice(0,1)
+// let d = a.slice(1,2)
+//
+// console.log(c + b + d)
+
+// ===========5===========
+
+// ===========6===========
+
+// let a = 'sunday'
+// let b = a.indexOf('d')
+//
+// console.log(b)
+
+// ===========6===========
+
+// ===========7===========
+
+// let a = 'hello'
+// let b = a.replace('l', 'k')
+// let c = b.replace('l','s')
+// console.log(c)
+
+// ===========7===========
+
+// ===========8===========
+
+// let a = 'user'
+// let b = 'new'
+// let c = a.slice(0 , 2)
+//
+// console.log(b + ' ' + c)
+
+// ===========8===========
+
+// ===========9===========
+
+// let a = 'monday'
+// let b = 'day'
+// let c = a.slice(0,3)
+//
+// console.log(b+b+c)
+
+// ===========9===========
+
+// ===========10===========
+
+// let a = 'HELLO'
+//
+// console.log(a[0].toLowerCase() + a[1].toUpperCase() + a[2].toLowerCase() + a[3].toUpperCase() + a[4].toLowerCase())
+
+// ===========10===========
+
+// ===========11===========
+
+// let a = 5
+// let b = '400'
+// let c = b.replace('0','5')
+//
+// console.log(c)
+
+// ===========11===========
+
+// ===========12===========
+
+// let a = 'monkey'
+// let b = 'banana'
+// let c = a.slice(0,1)
+// let d = a.slice(3,4)
+//
+// console.log(c + b.slice(3,6) + d)
+
+// ===========12===========
